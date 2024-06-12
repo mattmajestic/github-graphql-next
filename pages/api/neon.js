e8bc0c3747b977e06c81868183481a9bc113607b
Matt Majestic <33904170+mattmajestic@users.noreply.github.com>
2024-06-12 12:19:51 -0400

    remove geoip

--- pages/api/neon.js
+++ pages/api/neon.js
@@ -1,7 +1,6 @@
 // pages/api/neon.js
 import { Pool } from 'pg';
 import UAParser from 'ua-parser-js';
-import geoip from 'geoip-lite';
 
 let { PGHOST, PGDATABASE, PGUSER, PGPASSWORD } = process.env;
 
@@ -16,7 +15,6 @@ const pool = new Pool({
   },
 });
 
-
 export default async function handler(req, res) {
   // Get the device type from the user agent
   const userAgent = req.headers['user-agent'];
@@ -30,18 +28,12 @@ export default async function handler(req, res) {
     deviceType = 'unknown';
   }
 
-  // Get the client's IP address
-  const ip = (req.headers['x-forwarded-for'] || '').split(',').pop().trim() || 
-         req.connection.remoteAddress || 
-         req.socket.remoteAddress || 
-         req.connection.socket.remoteAddress;
-
-  // Get the geolocation data for the IP address
-  const geo = geoip.lookup(ip);
+  // Set the location to null
+  const location = null;
 
   // Insert a new row into the nextjs table
   const currentTime = new Date();
-  await pool.query("INSERT INTO nextjs (time, device_type, location) VALUES ($1, $2, $3)", [currentTime, deviceType, geo]);
+  await pool.query("INSERT INTO nextjs (time, device_type, location) VALUES ($1, $2, $3)", [currentTime, deviceType, location]);
 
   // Query the latest row from the nextjs table
   const queryResult = await pool.query("SELECT * FROM nextjs ORDER BY time DESC LIMIT 1");
